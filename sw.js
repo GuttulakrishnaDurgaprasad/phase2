4ef5029bcda8ef73358302a7308881153a2e7e58
GuttulakrishnaDurgaprasad <47556501+GuttulakrishnaDurgaprasad@users.noreply.github.com>
2019-02-21 22:18:05 +0530

    replace file sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,25 @@
-self.addEventListener('install',function(event){
-  event.waitUntil(
-    caches.open('v1').then(function(cache){
-      retuurn cache.addAll([
+var cache1="c1";
+this.addEventListener("install",function(event){
+	event.waitUntil(
+		caches.open(cache1).then(cache=>{
+			cache.addAll([
+				])
+		})
 
-      ]);
-    })
-  );
+
+		);
 });
-self.addEventListener('fetch',function(event){
-  event.respondWidth(
 
-  )
-})
+
+var cache1="c1";
+this.addEventListener("fetch",function(event){
+	event.respondWith(
+		caches.open(cache1).then(function(cache){
+			return cache.match(event.request).then(function(res){
+				return res || fetch (event.request).then(function(reqres){
+					cache.put(event.request,reqres.clone());
+          });
+        });
+      })
+    );
+});
